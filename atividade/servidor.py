20276083edc8dd6c171a7193d3545ca8f83d6565
miguelschuldt <miguelizador@gmail.com>
2019-05-08 09:17:37 -0300

    atualizando repositorio para trimestral

--- atividade/servidor.py
+++ atividade/servidor.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect , url_for, session
 
-app=Flask(__name__)
+app = Flask(__name__)
+app.config ["SECRET_KEY"] = "bla"
 
 class Pessoa:
     def __init__(self,nome,endereco,telefone):
@@ -10,10 +11,7 @@ class Pessoa:
 
 
 
-lista_de_pessoas=[Pessoa("Joao","Rua da arvore","33447698"),
-                  Pessoa("Brisola","Sem casa","2244552367"),
-                  Pessoa("Dj guuga","Na sua cama","6783346"),
-                  Pessoa("Kapela","Sem dona","171")]
+lista_de_pessoas=[]
 
 
 @app.route("/")
@@ -22,34 +20,76 @@ def inicio():
 
 @app.route("/listar_pessoas")
 def listar_pessoas():
-    return render_template("listar_pessoas.html",geral = lista_de_pessoas)    
+    return render_template("listar_pessoas.html", geral = lista_de_pessoas)    
 
 @app.route("/inserir_pessoa")
 def inserir_pessoas():
     return render_template("form_inserir_pessoa.html")  
 
-@app.route("/alterar_pessoas")
-def alterar_pessoas():
-    return render_template("form_alterar_pessoa.html")  
+@app.route("/form_alterar_pessoa")
+def for_alterar_pessoa():
+    procurado = request.args.get("nome")
+    for pe in lista_de_pessoas:
+        if pe.nome == procurado:
+            return render_template("form_alterar_pessoa.html", informacoes = pe)
+    return "Não achei: " + procurado + " :("
 
-@app.route("/exibir_mensagem")
-def exibir_mensagem():
+@app.route("/alterar_pessoa", methods = ['POST'])
+def alterar_pessoa():
+    procurado = request.form["nome_original"]
+    nome = request.form ["nome"]
+    endereco = request.form ["endereco"]
+    telefone = request.form ["telefone"]
+    fenix = Pessoa (nome, endereco, telefone)
+    for i in range(len(lista_de_pessoas)):
+        if lista_de_pessoas[i].nome == procurado:
+            lista_de_pessoas[i] = fenix
+            return redirect (url_for("listar_pessoas"))
+    return "Não achei: " + procurado + " :("
+
+@app.route("/cadastrar_pessoa", methods = ['POST'])
+def add():
+    endereco = request.form["endereco"]
+    nome = request.form["nome"]
+    telefone = request.form["telefone"]
+    lista = ([Pessoa(nome,endereco,telefone)])
+    lista_de_pessoas.append(Pessoa(nome,endereco,telefone))
+    #return render_template ("listar_pessoas.html", teste = lista, geral = lista_de_pessoas)
+    return redirect ( url_for ("listar_pessoas"))
+
+@app.route("/excluir_pessoa")
+def excluir_pessoa():
+    achou = None
+    nome = request.args.get("nome")
+    for p in lista_de_pessoas:
+        if p.nome == nome:
+            achou = p 
+            break
+    if achou != None:
+        lista_de_pessoas.remove(achou) 
     return render_template("exibir_mensagem.html")
 
-@app.route("/excluir_pessoa")    
-def excluir_pessoa():
-   nome = request.args.get("nome")
-   for p in lista_de_pessoas:
-       if p.nome == nome:
-           lista_de_pessoas.remove(p.nome)
-           break
 
-@app.route("/cadastrar_pessoa")
-def add():
-    endereco = request.args.get("endereco")
-    nome = request.args.get("nome")
-    lista = ([Pessoa(nome,endereco,"23454")])
-    lista_de_pessoas.append(Pessoa(nome,endereco,"23454"))
-    return render_template ("listar_pessoas.html", teste = lista, geral = lista_de_pessoas)
-    
-app.run()
+@app.route("/form_login")
+def form_login():
+    return render_template("form_login.html")  
+
+@app.route("/login", methods = ['POST'])
+def login():
+    login = request.form["login"]
+    senha = request.form["senha"]
+    if login == "franke" and senha == "123":
+        session ["usuario"] = login
+        return redirect("/listar_pessoas")
+    else:
+        return "Login/ senha inválida"
+
+@app.route("/logout")
+def logout():
+    session.pop("usuario")
+    return redirect("/")
+
+app.run( debug = True)
+
+
+
